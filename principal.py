265db3a78d79a8e6eca89133cab61283ad287501
MarcosDAguirre <pepetragoncete@gmail.com>
2026-09-20 12:39:47 -0300

    agrego archivos principal.py y estado_tiempo20260920.txt

---
+++ principal.py
@@ -0,0 +1,20 @@
+salida = {}              # fecha y hora en datetime
+with open ('estado_tiempo20260910.txt', encoding = 'cp1252') as f:
+    for i in f:
+        linea = i.split(';')
+        print (linea)
+        clave = linea[0].strip()  # clave es una ciudad.
+        fecha = linea[1]
+        hora = linea[2]
+        c_cielo = linea[3]
+        visibilidad = linea[4]
+        temperatura = float(linea[5])
+        s_térmica = linea[6]
+        humedad = linea[7]
+        viento = linea[8]
+        presion = linea[9].strip()
+
+        salida[clave] = {'Fecha':fecha,'Hora':hora,'Condición del cielo':c_cielo,'Visibilidad':visibilidad,'Temperatura':temperatura,'Sensación térmica':s_térmica,'Humedad':humedad,'Viento':viento,'Presión':presion}
+print (salida)        
+
+print (salida['Azul']['Visibilidad'])
