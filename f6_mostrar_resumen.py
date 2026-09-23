6a39bbbc8223e7a19637e8a427436a3a40172b00
MarcosDAguirre <pepetragoncete@gmail.com>
2026-09-23 18:24:11 -0300

    modifico f6_mostrar_resumen.py para incorporarle f8

--- f6_mostrar_resumen.py
+++ f6_mostrar_resumen.py
@@ -6,7 +6,7 @@ from f3_cantidad_de_ciudades import salida_f3
 from f4_cantidad_de_ciudades_completas import salida_f4
 from f5_top_n_ciudades import salida_f5
 from f7_ciudades_temperatura import salida_f7
-
+from f8_ciudades_viento import salida_f8
 
 def salida_f6():
 	# Parametros de entrada
@@ -22,6 +22,7 @@ def salida_f6():
 	cantidad_ciudades_completas = salida_f4(diccionario)
 	top_n_ciudades = salida_f5(diccionario,par_tipo,par_cantidad,par_sentido)
 	ciudades_temp = salida_f7(diccionario,cantidad_ciudades)
+	ciudades_vel = salida_f8(diccionario,cantidad_ciudades)
 	# Para imprimir 
 	print ('=' * 197 + '\nf6_mostrar_resumen')
 	print ('=' * 197 + '\nf1_leer_observaciones:')
@@ -36,6 +37,8 @@ def salida_f6():
 	print (f'f5_top_n_de_ciudades:\n {top_n_ciudades}')
 	print ('=' * 197 + '\nf7_ciudades_temperatura:')
 	print (json.dumps(ciudades_temp,indent = 4, ensure_ascii = False))
+	print ('=' * 197 + '\nf8_ciudades_viento:')
+	print (json.dumps(ciudades_vel,indent = 4, ensure_ascii = False))
 
 # Imprime por pantalla el resumen con todas las 
 # características calculadas. Usar n=5
