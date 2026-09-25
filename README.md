5b7ac0d5f0f8aa49c8cbc25b99b60d2772a781e0
MarcosDAguirre <pepetragoncete@gmail.com>
2026-09-24 21:29:09 -0300

    corrigo errores en f7 y f8. Agrego type hints y firmas a todas las funciones. Modifico archivo README actualizando el nombre de las funciones

--- README.md
+++ README.md
@@ -4,35 +4,35 @@ Para iniciar, ejecutar en la consola (a modo de ejemplo):
 ```bash
 python3 analisis_smn.py estado_tiempo20260910.txt
 ```
-El proyecto contiene 8 funciones en total. Al ejecutar " ~$ python3 analisis_smn.py estado_tiempo20260910.txt analisis_smn.py "; el módulo analisis_smn.py, llama a la funcion f6_mostrar_resumen.py. Esta función solicita los parámetros necesarios (empleando int(input('texto'))), para las otras funciones. Luego  ejecuta a cada una de ellas y finalmente imprime en pantalla los valores solicitados según consignas del trabajo práctico.
+El proyecto contiene 8 funciones en total. Al ejecutar " ~$ python3 analisis_smn.py estado_tiempo20260910.txt analisis_smn.py "; el módulo analisis_smn.py, llama a la función salida_f6 del archivo funciones.py Esta función solicita los parámetros necesarios (empleando int(input('texto'))), para las otras funciones. Luego  ejecuta a cada una de ellas y finalmente imprime en pantalla los valores solicitados según consignas del trabajo práctico.
 
 El proyecto contiene 8 funciones en total:
 ```bash
-f1_leer_observaciones.py
-f2_separar_viento.py
-f3_cantidad_de_ciudades.py
-f4_cantidad_de_ciudades_completas.py
-f5_top_n_ciudades.py
-f6_mostrar_resumen.py
-f7_ciudades_temperatura.py
-f8_ciudades_viento.py
+salida_f1
+salida_f2
+salida_f3
+salida_f4
+salida_f5
+salida_f6
+salida_f7
+salida_f8
 ```
-##                              Descripción de las funciones:
-### f1:
+## Descripción de las funciones:
+### salida_f1:
 Lee el archivo de observaciones del SMN y devuelve un diccionario  {ciudad: datos}, con los nombres de ciudad limpios y el campo de viento ya separado en dirección  y velocidad.
-### f2:
+### salida_f2:
 Convierte un campo de viento como 'Norte  3' en (direccion, velocidad). Contempla el caso 'Calma' (sin velocidad numérica).
-### f3:
+### salida_f3:
 Devuelve la cantidad total de ciudades leídas.
-### f4:
+### salida_f4:
 Devuelve la cantidad de ciudades sin ningún dato faltante.
-### f5:
+### salida_f5:
 Devuelve las n (por parámetro) ciudades ordenadas según 'campo', de mayor a menor (o al revés). Reutilizable tanto para temperatura como para viento.
-### f6:
+### salida_f6:
 Imprime por pantalla las características solicitadas.
-### f7:
+### salida_f7:
 Devuelve la/s ciudad(es)[:n] con la temperatura máxima y con la temperatura mínima.
-### f8:
+### salida_f8:
 Devuelve la/s ciudad(es)[:n] con velocidades máximas y mínimas de viento.
 
 
--- funciones.py
+++ funciones.py
@@ -3,49 +3,51 @@ import json
 from datetime import date, datetime , timedelta
 
 meses = {'enero':1,'febrero':2,'marzo':3,'abril':4,'mayo':5,'junio':6,'julio':7,'agosto':8,'septiembre':9,'octubre':10,'noviembre':11,'diciembre':12}
-def salida_f1(dato):
-	salida = {}
-	con_errores_1 = []
-	with open (dato, encoding = 'cp1252') as f:
-		for i in f:
-			linea = i.split(';')
-			if len(linea) == 10:
-				ciudad = linea[0].strip()  
-				fecha = linea[1].split('-')
-				fecha[1] = str(meses[fecha[1]])
-				fecha = '/'.join(fecha)  
-				fecha = datetime.strptime(fecha, "%d/%m/%Y").strftime('%d/%m/%Y')  
-				hora = datetime.strptime(linea[2],'%H:%M').strftime('%H:%M')
-				c_cielo = linea[3]
-				visibilidad = linea[4]
-				temperatura = float(linea[5])
-				s_térmica = linea[6]           
-				humedad = linea[7]
-				presion = linea[9].strip()
-				viento_v1 = linea[8].split()
-				if viento_v1[0] == 'Calma':
-					direccion_viento = 'Calma'
-					velocidad_viento = ' '
-				elif viento_v1[0] == 'Direcciones':
-					direccion_viento = 'Direcciones Variables'
-					velocidad_viento = viento_v1[2]
-				else:
-					direccion_viento = viento_v1[0]
-					velocidad_viento = viento_v1[1]           
-				salida[ciudad] = {'Fecha':fecha,'Hora':hora,'Condición del cielo':c_cielo,'Visibilidad':visibilidad,'Temperatura':temperatura,'Sensación térmica':s_térmica,'Humedad':humedad,'Direccion_viento': direccion_viento,'Velocidad_viento':velocidad_viento,'Presión':presion}    
-	return salida
-# Lee el archivo de observaciones del SMN y devuelve un diccionario
-# {ciudad: datos}, con los nombres de ciudad limpios y el campo de viento
-# ya separado en dirección y velocidad."""
+def salida_f1(dato: str) -> dict:
+    # Lee el archivo de observaciones del SMN (.txt) y devuelve un diccionario
+    # {ciudad: datos}, con los nombres de ciudad limpios y el campo de viento
+    # ya separado en dirección y velocidad."""
+    salida = {}
+    con_errores_1 = []
+    with open (dato, encoding = 'cp1252') as f:
+        for i in f:
+            linea = i.split(';')
+            if len(linea) == 10:
+                ciudad = linea[0].strip()  
+                fecha = linea[1].split('-')
+                fecha[1] = str(meses[fecha[1]])
+                fecha = '/'.join(fecha)  
+                fecha = datetime.strptime(fecha, "%d/%m/%Y").strftime('%d/%m/%Y')  
+                hora = datetime.strptime(linea[2],'%H:%M').strftime('%H:%M')
+                c_cielo = linea[3]
+                visibilidad = linea[4]
+                temperatura = float(linea[5])
+                s_térmica = linea[6]           
+                humedad = linea[7]
+                presion = linea[9].strip()
+                viento_v1 = linea[8].split()
+                if viento_v1[0] == 'Calma':
+                    direccion_viento = 'Calma'
+                    velocidad_viento = 0.0
+                elif viento_v1[0] == 'Direcciones':
+                    direccion_viento = 'Direcciones Variables'
+                    velocidad_viento = float(viento_v1[2])
+                else:
+                    direccion_viento = viento_v1[0]
+                    velocidad_viento = float(viento_v1[1])           
+                salida[ciudad] = {'Fecha':fecha,'Hora':hora,'Condición del cielo':c_cielo,'Visibilidad':visibilidad,'Temperatura':temperatura,'Sensación térmica':s_térmica,'Humedad':humedad,'Direccion_viento': direccion_viento,'Velocidad_viento':velocidad_viento,'Presión':presion}    
+    return salida
 
-def salida_f2(diccionario):
+def salida_f2(diccionario: dict) -> dict:
+    # Convierte un campo de viento como 'Norte  3' en (direccion, velocidad).
+    # Contempla el caso 'Calma' (sin velocidad numérica)
     ciudades_leidas = {}
     contador = 0
     for clave,valor in diccionario.items():
         valor_velocidad = valor['Velocidad_viento']
         if valor['Direccion_viento'] == 'Calma':
                 direccion_viento = 'Inexistente'
-                velocidad_viento = '0'
+                velocidad_viento = 0.0
         elif valor['Direccion_viento'] == 'Direcciones Variables':
                 direccion_viento = 'Variable'
                 velocidad_viento = valor_velocidad
@@ -54,10 +56,9 @@ def salida_f2(diccionario):
                 velocidad_viento = valor_velocidad
         ciudades_leidas[clave] = {'Direccion_viento':direccion_viento,'Velocidad_viento':velocidad_viento}
     return ciudades_leidas	
-# Convierte un campo de viento como 'Norte  3' en (direccion, velocidad).
-# Contempla el caso 'Calma' (sin velocidad numérica)	
 
-def salida_f3(diccionario):
+def salida_f3(diccionario: dict) -> dict:
+    # Devuelve la cantidad total de ciudades leídas.
     ciudades_leidas = []
     contador = 0
     total_ciudades = len(diccionario)
@@ -68,10 +69,9 @@ def salida_f3(diccionario):
         return {'Cantidad_de_ciudades_leidas':contador,'Ciudades_leidas':ciudades_leidas}
     else:
         return "Error en cálculo de ciudades_leidas"
-# Devuelve la cantidad total de ciudades leídas.
 
-
-def salida_f4(diccionario):
+def salida_f4(diccionario:dict) -> dict:
+    # Devuelve la cantidad de ciudades sin ningún dato faltante.
     ciudades_incompletas = []
     cantidad_de_ciudades = len(diccionario)
     for clave,valor in diccionario.items():
@@ -89,11 +89,13 @@ def salida_f4(diccionario):
     ciudades_completas = set(diccionario) - ciudades_incompletas_conjunto
     cantidad_completas = cantidad_de_ciudades - cantidad_de_incompletas
     return {'Cantidad_ciudades_completas':cantidad_completas,'Ciudades_completas':ciudades_completas}
-# Devuelve la cantidad de ciudades sin ningún dato faltante.
 
 salida = []
 lista_1 = []
-def salida_f5(diccionario,tipo,cantidad,sentido):           # 1 Temperatura / 2 Viento / 3 Ascendente / 4 Descendente
+def salida_f5(diccionario: dict,tipo: int,cantidad: int,sentido: int) -> list:          # 1 Temperatura / 2 Viento / 3 Ascendente / 4 Descendente
+    # Devuelve las n (por parámetro) ciudades ordenadas según 'campo', de mayor a menor
+    # (o al revés si descendente=False), en una lista. Reutilizable tanto para temperatura
+    # como para viento.
     for clave,valor in diccionario.items():
             if tipo == 1:
                 valor_temp = valor['Temperatura']
@@ -104,9 +106,9 @@ def salida_f5(diccionario,tipo,cantidad,sentido):           # 1 Temperatura / 2 
                     if valor_v1 == 'Calma':
                         valor_velocidad_viento = 0.0
                     elif valor_v1 == 'Direcciones Variables':
-                        valor_velocidad_viento = float(valor['Velocidad_viento'])
+                        valor_velocidad_viento = valor['Velocidad_viento']
                     else:
-                        valor_velocidad_viento = float(valor['Velocidad_viento'])
+                        valor_velocidad_viento = valor['Velocidad_viento']
                 except(IndexError):
                     pass
                 salida.append((valor_velocidad_viento,clave))
@@ -115,73 +117,22 @@ def salida_f5(diccionario,tipo,cantidad,sentido):           # 1 Temperatura / 2 
     if sentido == 4:
         lista_1 = ((sorted(salida))[::-1])
     return (lista_1[:cantidad])                    
-# Devuelve las n (por parámetro) ciudades ordenadas según 'campo', de mayor a menor
-# (o al revés si descendente=False), en una lista. Reutilizable tanto para temperatura
-# como para viento.
 
-salida1 = []
-sal_min1 = {}
-sal_max1 = {}
-def salida_f7(diccionario,cant):           
-    for clave,valor in diccionario.items():
-                valor_temp = valor['Temperatura']
-                salida1.append((valor_temp,clave))
-    temp_min = (sorted(salida))[:cant]
-    temp_max = ((sorted(salida))[::-1])[:cant]
-        
-    for par in temp_min:
-            sal_min1[par[1]] = par[0]
-    for par in temp_max:
-            sal_max1[par[1]] = par[0] 
-    d_salida = {'Temperaturas_minimas':sal_min1,'Temperaturas_maximas':sal_max1}       
-    return d_salida
-# ciudad(es)[:n] con la temperatura máxima y con la temperatura mínima.
-
-salida2 = []
-sal_min2 = {}
-sal_max2 = {}
-def salida_f8(diccionario,cant):
-    ciudades_leidas = {}
-    contador = 0
-    for clave,valor in diccionario.items():
-        valor_direccion = valor['Direccion_viento']
-        try:
-            if valor_direccion == 'Calma':
-                direccion_viento = 'Inexistente'
-                velocidad_viento = 0.0
-            elif valor_direccion == 'Direcciones':
-                direccion_viento = 'Variable'
-                velocidad_viento = float(valor['Velocidad_viento'])
-            else:
-                direccion_viento = valor_direccion
-                velocidad_viento = float(valor['Velocidad_viento'])
-        except(IndexError):
-            pass      
-        salida2.append((velocidad_viento,clave))
-    vel_min = (sorted(salida))[:cant]
-    vel_max = ((sorted(salida))[::-1])[:cant]
-    for par in vel_min:
-            sal_min2[par[1]] = par[0]
-    for par in vel_max:
-            sal_max2[par[1]] = par[0]
-    v_salida = {'Velocidades_minimas_viento':sal_min2,'Velocidades_maximas_viento':sal_max2}     
-    return v_salida
-
-def salida_f6():
-	# Parametros de entrada
+def salida_f6() -> None:
+    # Imprime por pantalla el resumen con todas las características solicitadas.
+    # Parametros de entrada
 	datos_SMN = sys.argv[1]
 	par_tipo = int(input('Para la funcion f5 eliga tipo escribiendo: 1 (para temperatura) o 2 (para viento): '))
 	par_cantidad = int(input('Para la funcion f5 ingrese la cantidad de ciudades con un numero entero: ')) 
 	par_sentido = int(input('Para la funcion f5 eliga tipo de orden escribiendo: 3 (para ascendente) o 4 (para descendente): '))
-	cantidad_ciudades = int(input('Para las funciones f7 y f8 ingrese un número entero para visualizar esa cantidad de ciudades: '))
 	# Llamadas a funciones
 	diccionario = salida_f1(datos_SMN)
 	viento = salida_f2(diccionario)
 	cantidad_ciudades_leidas = salida_f3(diccionario)
 	cantidad_ciudades_completas = salida_f4(diccionario)
 	top_n_ciudades = salida_f5(diccionario,par_tipo,par_cantidad,par_sentido)
-	ciudades_temp = salida_f7(diccionario,cantidad_ciudades)
-	ciudades_vel = salida_f8(diccionario,cantidad_ciudades)
+	ciudades_temp = salida_f7(diccionario)
+	ciudades_vel = salida_f8(diccionario)
 	# Para imprimir 
 	print ('=' * 197 + '\nf6_mostrar_resumen')
 	print ('=' * 197 + '\nf1_leer_observaciones:')
@@ -195,8 +146,22 @@ def salida_f6():
 	print ("=" * 197)
 	print (f'f5_top_n_de_ciudades:\n {top_n_ciudades}')
 	print ('=' * 197 + '\nf7_ciudades_temperatura:')
-	print (json.dumps(ciudades_temp,indent = 4, ensure_ascii = False))
+	print (ciudades_temp)
 	print ('=' * 197 + '\nf8_ciudades_viento:')
-	print (json.dumps(ciudades_vel,indent = 4, ensure_ascii = False))
-# Imprime por pantalla el resumen con todas las 
-# características calculadas. Usar n=5
+	print (ciudades_vel)
+
+def salida_f7(diccionaro:dict) -> dict:
+    #Devuelve la/s ciudad(es)[:n] con la temperatura máxima y con la temperatura mínima. Nota: n = 5
+    temperaturas = []
+    for clave,valor in diccionaro.items():
+        valor_temperatura = valor['Temperatura']
+        temperaturas.append((valor_temperatura,clave))
+    return {'Temperaturas_minimas':(sorted(temperaturas))[:5],'Temperaturas_maximas':((sorted(temperaturas))[::-1])[:5]}
+
+def salida_f8(diccionario: dict) -> dict:
+    # Devuelve la/s ciudad(es)[:n] con velocidades máximas y mínimas de viento. Nota: n = 5
+    velocidades_viento = []
+    for clave,valor in diccionario.items():
+        valor_velocidad_viento = valor['Velocidad_viento']
+        velocidades_viento.append((valor_velocidad_viento,clave))
+    return {'Velocidades_minimas_viento':(sorted(velocidades_viento))[:5],'Velocidades_maximas_viento':((sorted(velocidades_viento))[::-1])[:5]}
